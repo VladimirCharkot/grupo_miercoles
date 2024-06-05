7eecd2a64bf0443d5bb93e105a25511983eff8fb
VladimirCharkot <vlad.chk@gmail.com>
2024-06-05 18:37:40 -0300

    Inserte comentarios

--- codigo.py
+++ codigo.py
@@ -4,3 +4,10 @@ class Persona:
     pass
 
 print("Hola grupo!")
+
+# git init - convertir una carpeta en repo
+
+# git status
+
+# git add - agregar archivos al stage
+# git commit -m "Mensaje" - commitea los cambios
